3b34af0989fe79e8453bb0ddc9ff3bfaa103d526
Adam Coldrick <adam@sotk.co.uk>
2015-09-26 11:19:03 +0100

    Get last build

--- js/main.js
+++ js/main.js
@@ -8,13 +8,22 @@ app.config(['$httpProvider', function($httpProvider) {
 
 app.controller('VisualisationController', ['$scope', '$http',
     function($scope, $http) {
+        function formatBuild(response) {
+            return {
+                success: response.data.text[response.data.text.length - 1] === 'successful',
+            };
+        }
+
         $scope.steps = [];
         $http.get('http://ciat.baserock.org:8010/json/builders')
             .then(function(builders) {
                 angular.forEach(builders.data, function(value, key) {
+                    var lastBuildID = value.cachedBuilds[value.cachedBuilds.length - 1];
+                    var buildsPath = 'http://ciat.baserock.org:8010/json/builders/' +
+                                     key + '/builds/' + lastBuildID;
                     var step = {
                         name: key,
-                        last_build: value.cachedBuilds[value.cachedBuilds.length - 1],
+                        lastBuild: $http.get(buildsPath).then(formatBuild),
                         data: value
                     }
                     $scope.steps.push(step);
