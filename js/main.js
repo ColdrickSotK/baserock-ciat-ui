c3f4797f90af344324bacbf4128279270be6d5db
Adam Coldrick <adam@sotk.co.uk>
2015-09-26 10:20:06 +0100

    Inject $http

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var app = angular.module('ciat', []);
 
-app.controller('VisualisationController', ['$scope',
-    function($scope) {
+app.controller('VisualisationController', ['$scope', '$http'
+    function($scope, $http) {
         $scope.builders = $http.get('http://ciat.baserock.org:8010/json/builders');
     }]);
