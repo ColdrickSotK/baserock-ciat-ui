00e24462beb1f972900561e527bc601affe59aca
Adam Coldrick <adam@sotk.co.uk>
2015-09-26 10:26:09 +0100

    Try to fix stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = angular.module('ciat', []);
 
-app.controller('VisualisationController', ['$scope', '$http'
+app.controller('VisualisationController', ['$scope', '$http',
     function($scope, $http) {
         $scope.builders = $http.get('http://ciat.baserock.org:8010/json/builders');
-    }]);
+    }
+]);
