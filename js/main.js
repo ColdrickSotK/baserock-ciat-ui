7235cccbb865bfdc2229f1015d8373258e532c32
Adam Coldrick <adam@sotk.co.uk>
2015-09-26 10:18:58 +0100

    Use AngularJS in index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 var app = angular.module('ciat', []);
+
+app.controller('VisualisationController', ['$scope',
+    function($scope) {
+        $scope.builders = $http.get('http://ciat.baserock.org:8010/json/builders');
+    }]);
