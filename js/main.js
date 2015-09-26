733679936ac62d0d11b8a46c48d0ce0d0e6fa2e1
Adam Coldrick <adam@sotk.co.uk>
2015-09-26 10:30:09 +0100

    Enable CORS properly

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 var app = angular.module('ciat', []);
 
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
 app.controller('VisualisationController', ['$scope', '$http',
     function($scope, $http) {
         $scope.builders = $http.get('http://ciat.baserock.org:8010/json/builders');
